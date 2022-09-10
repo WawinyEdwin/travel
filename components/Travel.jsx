0e8c454d343075f99840168305855122e4aa7298
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-10 15:38:08 +0300

    fix

--- components/Travel.jsx
+++ components/Travel.jsx
@@ -85,7 +85,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+export default function TravelScren() {
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
 
